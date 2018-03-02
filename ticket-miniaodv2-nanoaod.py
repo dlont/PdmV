4e68c2981ffed1a5f5f3993bac8b43fa8507333d
dlont <dlont88@gmail.com>
2018-03-02 14:46:48 +0100

    Update ticket-miniaodv2-nanoaod.py

--- ticket-miniaodv2-nanoaod.py
+++ ticket-miniaodv2-nanoaod.py
@@ -54,115 +54,7 @@ createTicket = False
 N_REQUESTS_PER_TICKET = 30
 
 #[2] Choose one campaign types
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80PremixPUMoriond17_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond2017NanoAODGS'
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80PremixPUMoriond17Nooutput_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond2017NooutputNanoAODGS'
-#ochain = 'chain_RunIIWinter15pLHE_flowLHE2Summer15GS_flowRunIISummer16DR80PremixPUMoriond17_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond2017NanoAODpLHE'
-#ochain = 'chain_RunIIWinter15wmLHE_flowLHE2Summer15GS_flowRunIISummer16DR80PremixPUMoriond17_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond2017NanoAODwmLHE'
-#ochain = 'chain_RunIISummer15wmLHEGS_flowRunIISummer16DR80PremixPUMoriond17_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond2017NanoAODwmLHEGS'
-#ochain = 'chain_RunIISummer15wmLHEGS_flowRunIISummer16DR80PremixPUMoriond17Nooutput_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond2017NooutputNanoAODwmLHEGS'
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80BSandPU2016_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80BSandPU2016PremixPUMoriond2017NanoAODGS'
 
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80FlatPU0to75TuneCP2RECO_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80CP2PremixPUMoriond2017NooutputNanoAODwmLHEGS'
-
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80FlatPU0to75TuneCP0RECO_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80CP0PremixPUMoriond2017NanoAODGS'
-
-
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80FlatPU0to75TuneCP5RECO_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80CP5PremixPUMoriond2017NanoAODGS'
-
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80FlatPU0to75TuneCUETP8M1_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80FlatPU0to75TuneCUETP8M1NanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80FlatPU0to75TuneCUETP8M2_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80FlatPU0to75TuneCUETP8M2NanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80FlatPU0to75TuneCUETP8M3_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80FlatPU0to75TuneCUETP8M3NanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80FlatPU0to75TuneCUETP8M4_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80FlatPU0to75TuneCUETP8M4NanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80NoPUHcalNZSRAW_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80NoPUHcalNZSRAWNanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80PUFlat0to50_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUFlat0to50NanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80PUFlat0to70_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUFlat0to70NanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80PUMoriond17_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUMoriond2017NanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80PremixPUMoriond17EGM0_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond17EGM0NanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80PremixPUMoriond17RAW_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PUMoriond17RAWNanoAODGS'
-#
-#ochain = 'chain_RunIISummer15GS_flowRunIISummer16DR80PremixPilot_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPilotNanoAODGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowLHE2Summer15GS_flowRunIISummer16DR80FlatPU28to62HcalNZSRAW_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80FlatPU28to62HcalNZSRAWNanoAODwmLHEGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowLHE2Summer15GS_flowRunIISummer16DR80PUFlat0to50_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUFlat0to50NanoAODwmLHEGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowLHE2Summer15GS_flowRunIISummer16DR80PUMoriond17_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUMoriond17NanoAODwmLHEGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowLHE2Summer15GS_flowRunIISummer16DR80PremixPUMoriond17_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond17NanoAODwmLHEGSreGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowRunIISummer16DR80BSandPU2016_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80BSandPU2016NanoAODwmLHEGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowRunIISummer16DR80FlatPU0to75TuneCUETP8M1_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUFlat0to75TuneCUETP8M1NanoAODwmLHEGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowRunIISummer16DR80FlatPU0to75TuneCUETP8M2_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUFlat0to75TuneCUETP8M2NanoAODwmLHEGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowRunIISummer16DR80FlatPU0to75TuneCUETP8M3_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUFlat0to75TuneCUETP8M3NanoAODwmLHEGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowRunIISummer16DR80FlatPU0to75TuneCUETP8M4_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUFlat0to75TuneCUETP8M4NanoAODwmLHEGS'
-#
-#ochain = 'chain_RunIISummer15wmLHEGS_flowRunIISummer16DR80PUMoriond17_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80PUMoriond17NanoAODwmLHEGS'
-#
-#ochain = 'chain_RunIISummer16FSPremix_flowRunIISummer16MiniAODFast'
-#dchain = 'RunIISummer16FSPreMixNanoAOD'
-#
-#ochain = 'chain_RunIIWinter15pLHE_flowLHE2Summer15GSNooutput_flowRunIISummer16DR80PremixPUMoriond17Nooutput_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond2017NooutputNanoAODpLHE'
-#
-#ochain = 'chain_RunIIWinter15wmLHE_flowLHE2Summer15GSBS2016_flowRunIISummer16DR80BSandPU2016_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80BSandPU2016NanoAODwmLHE'
-#
-#ochain = 'chain_RunIIWinter15wmLHE_flowLHE2Summer15GS_flowRunIISummer16DR80FlatPU28to62HcalNZSRAWAODSIM_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80FlatPU28to62HcalNZSRAWAODSIMNanoAODwmLHE'
-#
-#ochain = 'chain_RunIIWinter15wmLHE_flowLHE2Summer15GS_flowRunIISummer16DR80FlatPU28to62HcalNZSRAW_flowRunIISummer16MiniAODv2'
-#dchain = 'RunIISummer16DR80FlatPU28to62HcalNZSRAWNanoAODwmLHE'
-#
-#ochain = 'chain_RunIIWinter15wmLHE_flowLHE2Summer15GS_flowRunIISummer16DR80PremixPUMoriond17EGM0_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond17EGM0NanoAODwmLHE'
-#
-#ochain = 'chain_RunIIWinter15wmLHE_flowLHE2Summer15GS_flowRunIISummer16DR80PremixPUMoriond17Nooutput_flowRunIISummer16PremixMiniAODv2'
-#dchain = 'RunIISummer16DR80PremixPUMoriond17NooutputNanoAODwmLHE'
-#
 ochain = 'chain_RunIIWinter15wmLHE_flowLHE2Summer15GS_flowRunIISummer16DR80PremixPUMoriond17NooutputpfMETcut_flowRunIISummer16PremixMiniAODv2'
 dchain = 'RunIISummer16DR80PremixPUMoriond17NooutputpfMETcutNanoAODwmLHE'
 
@@ -175,23 +67,11 @@ dchain = 'RunIISummer16DR80PremixPUMoriond17NooutputpfMETcutNanoAODwmLHE'
 ticketfilename = dchain+'.json'
 
 collector=defaultdict(lambda : defaultdict( lambda : defaultdict( int )))
-#for cc in ccs:
+
 print 50*"-"
-#print cc['prepid']
-#pwgs.remove('B2G')
-#pwgs.remove('BPH')
-#pwgs.remove('BTV')
-#pwgs.remove('EXO')
-#pwgs.remove('HIG')
-#pwgs.remove('JME')
-#pwgs.remove('SMP')
-#pwgs.remove('SUS')
-#pwgs.remove('TOP')
-#pwgs.remove('TSG')
+
 for pwg in pwgs:
-    #if pwg != "TOP": continue
-        ## get all chains from that pwg in that chained campaign
-        #crs = mcm.getA('chained_requests', query='member_of_campaign=%s&pwg=%s'%(cc['prepid'],pwg))
+
     crs = mcm.getA('chained_requests', query='member_of_campaign=%s&pwg=%s'%(ochain,pwg))
     print "\t",pwg,":\t",len(crs)
     for cr in crs:
